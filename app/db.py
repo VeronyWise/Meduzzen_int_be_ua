66af36a4f05da4719a0289b6f7a57e23905bb3c2
VeronyWise <vira5volos@gmail.com>
2022-10-20 14:11:39 +0300

    be-4

--- app/db.py
+++ app/db.py
@@ -1,5 +1,7 @@
 import databases
 from sqlalchemy import create_engine, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 
 DATABASE_URL = "postgresql://postgres:password@postgres_med:5432/meduzz_db"
@@ -9,8 +11,16 @@ database = databases.Database(DATABASE_URL)
 
 #  creating the tables in the same Python file, but in production, 
 # you would probably want to create them with Alembic, integrated with migrations
-# engine = create_engine(
-#     DATABASE_URL, connect_args={"check_same_thread": False})
+# SQLAlchemy Engine that will interact with our dockerized PostgreSQL database
+engine = create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False})
 
-# metadata = MetaData()
-# metadata.create_all(engine)
+
+
+metadata = MetaData()
+metadata.create_all(engine)
+
+# ORM session factory bound to this engine,
+# and a base class for our classes definitions.
+Session = sessionmaker(bind=engine)
+Base = declarative_base()
---
+++ migrations/versions/b0558281688a_created_user_table.py
@@ -0,0 +1,24 @@
+"""created user table
+
+Revision ID: b0558281688a
+Revises: 
+Create Date: 2022-10-20 13:37:21.150848
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b0558281688a'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
